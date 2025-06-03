74d4336413310eae7ded4d51b397124b45f397de
Mareeee <markocvijanovicmlg@gmail.com>
2025-06-03 16:05:43 +0200

    [ADD] CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import home from './routes/home.js';
 import auth from './routes/auth.js';
 
 const app = express();
 app.use(express.json());
-app.use('/', home);
+app.use(cors());
+app.use('/home', home);
 app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
